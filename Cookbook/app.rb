5fe9964a620d54bcdfef4da9598ddf50578a8bf5
juan bernal <mesieou@gmail.com>
2023-12-08 08:07:52 +1100

    meal repository completed

--- Cookbook/app.rb
+++ Cookbook/app.rb
@@ -3,20 +3,7 @@ require_relative './app/repositories/meal_repository.rb'
 
 filepath = File.join(__dir__, 'data/meals.csv')
 
-puts "creating a rep"
-p meal_repo = MealRepository.new(filepath)
-puts "--------------"
-
-
-puts "creating a new burger"
-burger = Meal.new(id: 12, name: "Cheese burger", price: 235.8)
-p meal_repo.create(burger)
-puts "--------------"
-
-puts "seeing all burgers"
-meal_repo.all
-puts "--------------"
-
-puts "finding a burger"
-meal_repo.find(2)
-puts "--------------"
+meal_repo = MealRepository.new(filepath)
+hawaii_meal = Meal.new(price: 11, name: "Hawaii")
+meal_repo.create(hawaii_meal)
+p hawaii_meal.id == 1
